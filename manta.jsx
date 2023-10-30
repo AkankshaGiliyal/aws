12633c1189ca1a991f1d3c79b86271055d7936ec
Akanksha Giliyal <108074183+AkankshaGiliyal@users.noreply.github.com>
2023-10-30 15:33:33 +0530

    Update manta.jsx

--- manta.jsx
+++ manta.jsx
@@ -2,7 +2,7 @@ const express = require('express');
 const { ethers } = require('ethers');
 
 const app = express();
-const port = 3001;
+const port = 80;
 const path = require('path');
 const publicIP = '18.208.129.38';
 
